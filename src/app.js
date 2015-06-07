20dc739f0fb5bcf93402d37c3a8edfc6dbe8b856
Matita <matitald@gmail.com>
2015-06-07 23:38:58 +0200

    maze generator WIP

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 var gridMaker = require('./util/grid.js');
+var mazer = require('./util/maze-generator.js')
 
-console.log(gridMaker(10, 10));
+var grid = gridMaker(31, 31);
+mazer(grid);
+drawGrid(grid);
+console.log(grid);
+
+
+
+function drawGrid(grid) {
+  var html = grid.map(function(row) {
+    return row.join(' ');
+  }).join('<br>');
+
+  document.getElementById('canvas').innerHTML = html;
+}
